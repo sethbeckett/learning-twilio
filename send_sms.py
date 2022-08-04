d18f5d204f8890b5dedb9b15de8893b0a362750c
Seth Beckett <sethbeckett@Seths-MacBook-Pro.local>
2022-08-04 15:32:39 -0600

    successfully sent a text!

--- send_sms.py
+++ send_sms.py
@@ -1,12 +1,19 @@
-import os
+from email import message
 from twilio.rest import Client
 
 with open('credentials.txt') as file:
-    sid = file.readline()
-    token = file.readline()
+    account_sid = file.readline()
+    auth_token = file.readline()
+    my_twilio_num = file.readline()
+    recipient_num = file.readline()
 
-print(sid, token)
-# account_sid = os.environ['ACad9e4f80c786d3965ecb7b1ef54f9b97']
-# account
+client = Client(account_sid,auth_token)
 
-# os.environ[]
+message = client.messages \
+                .create(
+                    body="This is a test message from Seth using Twilio in a Python script",
+                    from_=my_twilio_num,
+                    to=recipient_num
+                )
+
+print(message.sid)
